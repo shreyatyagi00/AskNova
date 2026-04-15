0fd4d2233530031f3c383b2c4f534926789ee5e7
Shreya Tyagi <shreyaatyagii@gmail.com>
2026-04-16 00:29:17 +0530

    initial clean commit

---
+++ Backend/src/routes/auth.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { register, verifyEmail , login , getMe } from "../controllers/auth.controller.js";
+import { registerValidator, loginValidator } from "../validators/auth.validator.js";
+import { authUser } from "../middleware/auth.middleware.js";
+const authRouter = Router();
+
+authRouter.post("/register", registerValidator,register);
+authRouter.post("/login", loginValidator, login)
+authRouter.get('/get-me', authUser, getMe)
+authRouter.get('/verify-email', verifyEmail)
+
+export default authRouter;
