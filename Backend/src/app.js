81cdd4bf4adba0e205cab3337dc7bf6880429cb5
Shreya Tyagi <shreyaatyagii@gmail.com>
2026-04-16 15:57:45 +0530

    fix cors properly

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,6 +11,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
+import express from "express";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.routes.js";
+import chatRouter from "./routes/chat.routes.js";
+import morgan from "morgan";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan("dev"));
 app.use(cors({
     origin: [
         "http://localhost:5173",
@@ -18,6 +31,15 @@ app.use(cors({
     ],
     credentials: true,
 }))
+
+app.get("/", (req, res) => {
+    res.json({ message: "Server is running" });
+});
+
+app.use("/api/auth", authRouter);
+app.use("/api/chats", chatRouter);
+
+export default app;
 
 app.get("/", (req, res) => {
     res.json({ message: "Server is running" });
